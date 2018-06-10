98eda6e6ff7cc346e1ffad04aa538717742b5f01
Rahmi Pruitt <rjpruitt16@gmail.com>
2018-06-10 18:35:09 -0500

    Added email and password for Sign up feature
    Overading the create function in userSerializers, I manually create objects, and save to the user table. I also added password and username to the meta class

--- backend/snapbackend/api/serializers.py
+++ backend/snapbackend/api/serializers.py
@@ -6,7 +6,20 @@ from rest_framework import serializers
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ('url', 'username', 'email', 'groups')
+        fields = ('url', 'username', 'email', 'password')
+        write_only_fields = ('password',)
+
+    def create(self, validated_data):
+        user = User.objects.create(
+            username=validated_data['username'],
+            email=validated_data['email'],
+        )
+
+        user.set_password(validated_data['password'])
+        user.save()
+
+        return user
+
 
 
 class SnapCapsuleSerializer(serializers.HyperlinkedModelSerializer):
